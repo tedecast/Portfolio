f201acc63a99b7a51550c626bbe14ca3d19ded51
Teresa de Castro <decastro.ter@gmail.com>
2023-06-25 18:50:38 -0400

    Added in fonts

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,5 +1,9 @@
-import './sidebar.scss';
+import "./sidebar.scss";
 
-const Sidebar = () => (<>Sidebar</>)
+const Sidebar = () => (
+  <div className="nav-bar">
+    <h1 className="t-logo" to="/">T</h1>
+  </div>
+);
 
 export default Sidebar;
