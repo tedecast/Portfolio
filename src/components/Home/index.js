e3aba729c136c68b61616dfc171b9895fb960561
Teresa de Castro <decastro.ter@gmail.com>
2023-07-04 18:29:36 -0400

    Added to text-zone

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,6 +7,9 @@ const Home = () => {
       <div className="text-zone">
         <span className="hi-text">H</span>
         <span className="hi-text">i,</span>
+        <br />
+        <span>I'</span>
+        <span>m</span>
       </div>
     </div>
   );
