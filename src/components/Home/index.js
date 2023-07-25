d3e2e50224df0bf6649b6ed74d66ee7bce41c31e
Teresa de Castro <decastro.ter@gmail.com>
2023-07-24 20:29:17 -0400

    Got the text to animate

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,57 @@
 import { Link } from "react-router-dom";
 import "./index.scss";
 import Portrait from "./../../assets/images/Self-Portrait.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const emoji = "\u{2728}";
   const [letterClass, setLetterClass] = useState("text-animate");
   const nameArray = ["e", "r", "r", "i"];
+  const jobTitle = "Web Developer";
+  const jobArray = jobTitle
+    .split("")
+    .map((letter) => (letter === " " ? "\u00A0" : letter));
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <div className="home-page container">
       <div className="text-zone">
         <div>
-          <p className="hi-text">Hi,</p>
+          <p className={`hi-text ${letterClass}`}>H</p>
+          <p className={`hi-text ${letterClass} _12`}>i,</p>
           <br />
-          <p>I'm &nbsp;</p>
-          <p className="t-logo">T</p>
-          <p>erri</p>
-          <p className="emoji">{emoji}</p>
+          <p className={`${letterClass} _13`}>I'</p>
+          <p className={`${letterClass} _14`}>m &nbsp;</p>
+          <p className={`t-logo ${letterClass} _15`}>T</p>
+          {nameArray.map((letter, index) => (
+            <p
+              key={index}
+              className={`${letterClass} name-label _${index + 16}`}
+            >
+              {letter}
+            </p>
+          ))}
+          <p className={`emoji ${letterClass} name-label _20`}>{emoji}</p>
           <br />
-          <p className="job-title">Web Developer</p>
+          <p className="job-title">
+            {jobArray.map((letter, index) => (
+              <span
+                key={index}
+                className={`${letterClass} name-label _${index + 21}`}
+              >
+                {letter}
+              </span>
+            ))}
+            <br />
+          </p>
           <Link to="/contact" className="contact-button">
             CONTACT ME
           </Link>
